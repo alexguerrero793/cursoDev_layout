4ab216fffde766cee8d4579c013c5a01eaf51a92
Alex Guerrero <alexguerrero@Alexs-MBP.home>
2021-05-12 20:28:40 +0200

    icons

---
+++ src/app/cara7/cara7.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cara7',
+  templateUrl: './cara7.component.html',
+  styleUrls: ['./cara7.component.scss']
+})
+export class Cara7Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
